d82c4a1cd592b57ad3345e9c4aa2cd462552361a
ngshya <ngshya@gmail.com>
2020-03-30 14:19:01 +0200

    draft

--- scripts/draft.py
+++ scripts/draft.py
@@ -3,7 +3,12 @@ sys.path.append(".")
 import cProfile
 import pstats
 from covid19it.data.provincia import DataProvincia
+from covid19it.utils.get_corr_lag import get_corr_lag
 
+'''
 obj_data = DataProvincia()
 obj_data.load()
 obj_data.parse()
+'''
+
+print(get_corr_lag([0,3,4,4,0,0], [0,0,0,2,3,3]))
